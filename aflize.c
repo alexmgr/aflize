e53279c53d4adebd4acb46597d93e98d20ccc9ed
Alex Moneger <alexmgr+github@gmail.com>
2016-04-05 12:25:25 -0700

    Initial commit of AFL wrapper
    - Reads file content from path passed as argv[1] and puts it into
      a buffer
    - Passes the buffer over to a user defined function afl_forward

---
+++ aflize.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aflize.h"
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Insufficient number of arguments passed to AFL wrapper\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(f, 0L, SEEK_END);
+    long file_size = ftell(f);
+    rewind(f);
+
+    char buf[file_size + 1];
+    if (fread(buf, file_size, 1, f) != 1) {
+        fclose(f);
+        perror("fread");
+        exit(EXIT_FAILURE);
+    }
+    buf[file_size] = 0;
+    fclose(f);
+
+    afl_forward(buf);
+
+    return EXIT_SUCCESS;
+}
---
+++ aflize.h
@@ -0,0 +1,18 @@
+//
+// Created by Alex Moneger on 4/5/16.
+//
+
+#ifndef AFLIZE_AFLIZE_H
+#define AFLIZE_AFLIZE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void afl_forward(char* buf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //AFLIZE_AFLIZE_H
