fb3970b60a38cee5397eca6be194efe05699ecd4
Alex Moneger <alexmgr+github@gmail.com>
2016-05-24 14:33:47 -0700

    Fixed incorrect gitignore entry

---
+++ src/argvalize.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "aflize.h"
+
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Insufficient number of arguments passed to AFL wrapper\n";
+        std::cerr << "Expecting filename as first argument\n";
+        exit(EXIT_FAILURE);
+    }
+
+    const char *filename = argv[1];
+
+#ifdef AFLIZE_PERSIST
+    while (__AFL_LOOP(AFLIZE_PERSIST_ITERATIONS))
+#endif
+    {
+        std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
+        std::ifstream::pos_type file_size = ifs.tellg();
+
+        if (file_size > 0) {
+            std::vector<char> buffer(file_size);
+
+            ifs.seekg(0, std::ios::beg);
+            ifs.read(&buffer[0], file_size);
+
+            afl_forward(&buffer[0]);
+        } else if (file_size == 0) {
+            std::cerr << "Empty file: " << filename << "\n";
+            exit(EXIT_FAILURE);
+        } else {
+            std::cerr << "Unable to access: " << filename << "\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
